05667d46f3829e791b7af13252e25b0d90782f24
eeelsa1 <116863269+eeelsa1@users.noreply.github.com>
2024-05-10 00:10:01 +0800

    0

---
+++ src/components/WeatherApp/WeatherApp.jsx
@@ -0,0 +1,82 @@
+import React from "react";
+import "./WeatherApp.css";
+import searchIcon from "../Assets/search.png";
+import clear_icon from "../Assets/clear.png";
+import cloud_icon from "../Assets/cloud.png";
+import drizzle_icon from "../Assets/drizzle.png";
+import humidity_icon from "../Assets/humidity.png";
+import rain_icon from "../Assets/rain.png";
+import snow_icon from "../Assets/snow.png";
+import wind_icon from "../Assets/wind.png";
+
+const WeatherApp = async () => {
+  //async 非同步
+  let api_key = "7687a140640e6ced2ed0c8326eb7bb02";
+
+
+  const search = async () => {
+    const element = document.getElementsByClassName("cityInput");
+    if (element[0].value === "") {
+      return 0;
+    }
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${element[0].value}&units=Metric&appid=${api_key}`;
+
+
+    let response = await fetch(url); // await 只允許在非同步函式中與模組的最上層使用
+    let data = await response.json();
+
+    const humidity = document.getElementsByClassName("humidity-percent");
+    const wind = document.getElementsByClassName("wind-rate");
+    const temperature = document.getElementsByClassName("weather-temp");
+    const location = document.getElementsByClassName("weather-location");
+
+    humidity[0].innerHTML = data.main.humidity + "%";
+    wind[0].innerHTML = data.wind.speed + "km/h";
+    temperature[0].innerHTML = data.main.temp + "°C";
+    location[0].innerHTML = data.name;
+  }//
+
+  return (
+    <div className="container">
+      <div className="top-bar">
+        <input
+          type="text"
+          className="cityInput"
+          placeholder="Search the city"
+        />
+        <div
+          className="search-icon"
+          onClick={() => {
+            search();
+          }}
+        >
+          <img src={searchIcon} alt="" />
+        </div>
+      </div>
+      <div className="weather-img">
+        <img src={cloud_icon} alt="" />
+      </div>
+      <div className="weather-temp">24°C</div>
+      <div className="weather-location">London</div>
+      <div className="data-container">
+        <div className="element">
+          <img src={humidity_icon} alt="" className="icon" />
+          <div className="data">
+            <div className="humidity-percent">64%</div>
+            <div className="text">Humidity</div>
+          </div>
+        </div>
+
+        <div className="element">
+          <img src={wind_icon} alt="" className="icon" />
+          <div className="data">
+            <div className="wind-rate">18 km/h</div>
+            <div className="text">Wind Speed</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default WeatherApp;
